f9222c0ab2eb93d003e4ad3c45ec3c4eb150c9cc
sambaird <bairdsam@oregonstate.edu>
2022-05-27 12:56:42 -0600

    Added fields to and registered models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,9 +1,11 @@
-from tkinter import CASCADE
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+MAX_PRICE_DIGITS = 6  # Max price is $9,999.99
+
 class User(AbstractUser):
-    watchlist = models.ManyToManyField('Listing', blank=True, related_name="watchlist_users")
+    watchlist = models.ManyToManyField('Listing', blank=True, related_name="watchlister")
+    bids = models.ManyToManyField('Bid', blank=True, related_name="bidder")
 
 
 class Category(models.TextChoices):
@@ -17,19 +19,23 @@ class Category(models.TextChoices):
 class Listing(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=1000)
-    starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
+    start_price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=2, null=True)
     image = models.URLField(blank=True)
     category = models.CharField(choices=Category.choices, max_length=50, blank=True)
     is_active = models.BooleanField(default=True)
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="postings")
+    datetime = models.DateTimeField(auto_now_add=True, null=True)
     def __str__(self):
         string = ""
         string += "Active" if self.is_active else "Inactive"
         return f" listing: '{self.title}' by {self.poster} for ${self.starting_bid}"
 
 class Bid(models.Model):
-    pass
+    price = models.DecimalField(max_digits=MAX_PRICE_DIGITS, decimal_places=2, null=True)
+    datetime = models.DateTimeField(auto_now_add=True, null=True)
 
 class Comment(models.Model):
     text = models.CharField(max_length=500)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="comments")
+    datetime = models.DateTimeField(auto_now_add=True, null=True)
 
